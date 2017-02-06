3af3dd392c4b15e5c4ada17016ccd04e40e70317
Zoltan Batoczki <zoltanbatoczki@gmail.com>
2017-02-05 17:39:05 -0800

    Complete redesign using a client
    Was unsuccessful implementing using PuTTY

--- client.py
+++ client.py
@@ -2,16 +2,73 @@
 
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def play(connection):
+    playerMove = input("Choose rock, paper, or scissors --> ") #player makes a move
+    #client requests server move
+    connection.send('play'.encode('ascii'))
+    serverMove = connection.recv(1024).decode('ascii')
+    #reports move to user
+    print("Server played " + serverMove)
+    #determines outcome
+    if(playerMove == serverMove) :
+        results = 2 #draw
+    elif(playerMove == 'rock'):
+        if(serverMove == 'paper'):
+            results = 0 #player lost
+        else:
+            results = 1 #player won
+    elif(playerMove == 'paper'):
+        if(serverMove == 'scissors'):
+            results = 0
+        else:
+            results = 1
+    elif(playerMove == 'scissors'):
+        if(serverMove == 'rock'):
+            results = 0
+        else:
+            results = 1
+    return results
 
-host = socket.gethostname()
+def Main():
+    #tracks player score
+    wins = 0
+    losses = 0
+    draws = 0
 
-port = 9999
+    host = socket.gethostname()
+    port = 5000
 
-s.connect((host,port))
+    #attemptes to connect to server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host,port))
+    #recieve welcome message from server
+    data = s.recv(1024).decode('ascii')
+    print(data)
 
-msg = s.recv(1024) #recieve no more than 1024 bytes
+    #user gives input and client responds accordingly
+    userInput = input("-> ").lower()
+    while userInput != 'quit':
+        if(userInput == 'score'): #report score
+            msg = '---YOUR SCORE---\n WINS: %d | LOSSES: %d | DRAWS: %d\r\n' % (wins, losses, draws)
+            print(msg)
+        elif userInput == 'play': #play a game
+            result = play(s)
+            if result == 0:
+            	print("You lose!")
+            	losses += 1
+            elif result == 1:
+            	print("You won!")
+            	wins += 1
+            elif result == 2:
+            	print("It\'s a draw!")
+            	draws += 1
+        else:
+            print("Incorrect input. Please try again.")
+        print('>PLAY >SCORE >QUIT' + "\r\n")
+        userInput = input("-> ").lower()
 
-#s.close()
+    print("Goodbye!")
+    s.close()
 
-print(msg.decode('ascii'))
+if __name__ == '__main__':
+    Main()
--- server.py
+++ server.py
@@ -7,78 +7,46 @@
 import socket
 from random import randint
 
-def get_input() :
-    s = (clientSocket.recv(1024)).decode('ascii')
-    s = s.strip('\n')
-    s = s.lower() #set to lowercase
-    return s
-
-def play():
+def generateMove():
+    moves = {1:'rock', 2:'paper', 3:'scissors'}
     cpuInput = moves[randint(1,3)]
-    playerInput = get_input()
-    results = ''
-    if(playerInput == cpuInput) :
-        results = 'It\'s a draw!'
-    elif(playerInput == 'rock'):
-        if(cpuInput == 'paper'):
-            results = 'You lose!'
-        else:
-            results = 'You win!'
-            score += 1
-    elif(playerInput == 'paper'):
-        if(cpuInput == 'scissors'):
-            results = 'You lose!'
-        else:
-            results = 'You win!'
-            score += 1
-    elif(playerInput == 'scissors'):
-        if(cpuInput == 'rock'):
-            results = 'You lose!'
-        else:
-            results = 'You win!'
-            score += 1
+    return cpuInput
 
-moves = {1:'rock', 2:'paper', 3:'scissors'}
-score = 0
+def Main():
+    #create socket object
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#create socket object
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #get local host
+    host = socket.gethostname() #'192.168.1.2'
+    #print(host)
+    port = 5000
 
-#get local host
-host = socket.gethostname()
-print(host)
-port = 5000
+    #bind port
+    serverSocket.bind((host, port))
 
-#bind port
-serverSocket.bind((host, port))
+    #set number of connections to accept
+    serverSocket.listen(1) #only one connection will be accepted
 
-#set number of connections to accept
-serverSocket.listen(1) #only one connection will be accepted
+    #establish connection
+    print('Waiting for client...')
+    clientSocket, addr = serverSocket.accept()
+    print("Connected with " + str(addr))
+    msg = 'WELCOME!' + "\r\n" + '>PLAY >SCORE >QUIT' + "\r\n"
+    clientSocket.send(msg.encode('ascii'))
 
-#establish connection
-print('Waiting for client...')
-clientSocket, addr = serverSocket.accept()
-print("Got a connection from %s" % str(addr))
-#provide main menu
-#recieves main menu action 'PLAY' 'SCORE' or 'QUIT'
-msg = 'WELCOME!' + "\r\n" + '>PLAY >SCORE >QUIT' + "\r\n"
-clientSocket.send(msg.encode('ascii'))
+    while True: 
+              
+        data = clientSocket.recv(1024).decode('ascii')
+        if not data:
+            break
+        elif data == 'play':
+            print("From connected user: " + data)
+            move = generateMove()
+            print("Generated \'%s\'" % move)
+            clientSocket.send(move.encode('ascii'))
 
+    print("Closing connection from " + str(addr))
+    clientSocket.close()
 
-while True:
-    userInput = get_input()
-    if(userInput == 'quit') :
-        break
-    elif(userInput == 'score'):
-        msg = 'YOUR SCORE: %d\r\n' % score
-        clientSocket.send(msg.encode('ascii'))
-    elif(userInput == 'play') :
-        play()
-    else :
-        msg = 'Input error. Please try again\r\n'
-        clientSocket.send(msg.encode('ascii'))
-    
-goodbye = 'Thanks for playing!'
-clientSocket.send(goodbye.encode('ascii'))
-clientSocket.close()
-
+if __name__ == '__main__':
+    Main()
